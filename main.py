2dca4631a46af4c086ce8e60770c8a188cd71e3c
alexkrul0 <alexkrul0@gmail.com>
2024-08-31 13:33:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and  1000 != 1000.1)
+
+# 3nd program
+num1 = 2 * 2 + 2
+print(num1)
+num2 = 2 * (2 + 2)
+print(num2)
+print(num1 == num2)
+
+# 4nd program
+string = '123.456'
+num = float(string)
+num = num * 10
+num = int(num)
+print(num % 10)
